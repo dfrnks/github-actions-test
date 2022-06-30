31ed65edb70416fe4c92a316823a74c5a636a676
Douglas Francisco Cardoso <douglasfrancardoso@gmail.com>
2022-06-30 14:38:15 -0300

    Create our action

---
+++ .github/actions/hello/index.js
@@ -0,0 +1,18 @@
+const core = require("@actions/core")
+const github = require("@actions/github")
+
+try {
+
+    const name = core.getInput('who-are-you')
+
+    console.log('Hello, ${name}')
+    
+    const time = new Date();
+    
+    core.setOutput("time", time.toTimeString())
+    
+    console.log(JSON.stringify(github, null, '\t'))    
+} catch(error) {
+    core.setFailed('Deu erro, tente novamente mais tarde')
+    core.setFailed(error)
+}
